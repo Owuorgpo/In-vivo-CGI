198e9a6720ca38933a5261c1ea3e4314126c98ca
Peter O. Oluoch <owuorgpo@gmail.com>
2024-08-19 09:31:57 -0400

    Update 1_extract_barcode_counts.sh
    This script uses the barcodes.fa file to extract barcode counts from the fastq.gz files.

--- 1_extract_barcode_counts.sh
+++ 1_extract_barcode_counts.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 #build index
 subread-buildindex -o esx_rd2 barcodes.fa  #build index. barcodes.fa is fasta file with all strains and respective barcodes
 
@@ -9,21 +8,19 @@ for fastq_file in *.fastq.gz; do
     # Extract the part of the name until the third '_'
     output_prefix=$(echo "$fastq_file" | cut -d '_' -f 1-3)
     
-    # Define the output file names
+    # Output names
     aligned_bam="${output_prefix}_aligned.bam"
     sorted_bam="${output_prefix}_sorted.bam"
     idxstats_output="${output_prefix}_counts.txt"
     
-    # Perform the subread-align command
+    # Subread-align
     subread-align -i ../Broad_hypos -r "$fastq_file" -t 1 -o "$aligned_bam"
     
-    # Sort the aligned BAM file
+    # BAM file processing
     samtools sort -o "$sorted_bam" "$aligned_bam"
-    
-    # Index the sorted BAM file
     samtools index "$sorted_bam"
     
-    # Generate idxstats output
+    # Samtools counts
     samtools idxstats "$sorted_bam" > "$idxstats_output"
     
     echo "Aligned $fastq_file to $sorted_bam, indexed, and generated idxstats."
